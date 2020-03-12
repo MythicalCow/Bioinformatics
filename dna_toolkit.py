f0c778c4a9d853f11523876f3df5e46a932cb898
MythicalCow <62105787+MythicalCow@users.noreply.github.com>
2020-03-12 14:07:07 -0400

    Add files via upload

---
+++ dna_toolkit.py
@@ -0,0 +1,92 @@
+# DNA Toolkit file
+from structures import *
+from collections import Counter
+from Bio import ExPASy
+from Bio import SwissProt
+
+
+def validate_seq(seq):
+    """Check the sequence to make sure it is a valid DNA string"""
+
+    tmpseq = seq.upper()
+    for nuc in tmpseq:
+        if nuc not in DNA_Nucleotides:
+            return False
+    return tmpseq
+
+
+def nucleotide_frequency(seq):
+    """Count nucleotides in a given sequence. Return a dictionary"""
+
+    tmpFreqDict = {"A": 0, "C": 0, "G": 0, "T": 0}
+    for nuc in seq:
+        tmpFreqDict[nuc] += 1
+    return tmpFreqDict
+
+    # More Pythonic, using Counter
+    # return dict(Counter(seq))
+
+
+def transcription(seq):
+    """DNA -> RNA Transcription. Replacing Thymine with Uracil"""
+    return seq.replace("T", "U")
+
+
+def reverse_complement(seq):
+    """
+    Swapping adenine with thymine and guanine with cytosine.
+    Reversing newly generated string
+    """
+    return ''.join([DNA_ReverseComplement[nuc] for nuc in seq])[::-1]
+
+    # Pythonic approach. A little bit faster solution.
+    # mapping = str.maketrans('ATCG', 'TAGC')
+    # return seq.translate(mapping)[::-1]
+
+
+def gc_content(seq):
+    """GC Content in a DNA/RNA sequence"""
+    return round((seq.count('C') + seq.count('G')) / len(seq) * 100)
+
+
+def gc_content_subsec(seq, k=20):
+    """GC Content in a DNA/RNA sub-sequence length k. k=20 by default"""
+    res = []
+    for i in range(0, len(seq) - k + 1, k):
+        subseq = seq[i:i + k]
+        res.append(gc_content(subseq))
+    return res
+
+
+def translate_seq(seq, init_pos=0):
+    """Translates a DNA sequence into an aminoacid sequence"""
+    return [DNA_Codons[seq[pos:pos + 3]] for pos in range(init_pos, len(seq) - 2, 3)]
+
+
+def codon_usage(seq, aminoacid):
+    """Provides the frequency of each codon encoding a given aminoacid in a DNA sequence"""
+    tmpList = []
+    for i in range(0, len(seq) - 2, 3):
+        if DNA_Codons[seq[i:i + 3]] == aminoacid:
+            tmpList.append(seq[i:i + 3])
+
+    freqDict = dict(Counter(tmpList))
+    totalWight = sum(freqDict.values())
+    for seq in freqDict:
+        freqDict[seq] = round(freqDict[seq] / totalWight, 2)
+    return freqDict
+
+
+def gen_reading_frames(seq):
+    """Generate the six reading frames of a DNA sequence, including reverse complement"""
+    frames = []
+    frames.append(translate_seq(seq, 0))
+    frames.append(translate_seq(seq, 1))
+    frames.append(translate_seq(seq, 2))
+    return frames
+
+def proteindata():
+    handle = ExPASy.get_sprot_raw('B5ZC00') #you can give several IDs separated by commas
+    record = SwissProt.read(handle) # use SwissProt.parse for multiple proteins
+    return record
+
---
+++ main.py
@@ -0,0 +1,38 @@
+# DNA Toolset/Code testing file
+from dna_toolkit import *
+import random
+
+# Creating a random DNA sequence for testing:
+randDNAStr = ''.join([random.choice(DNA_Nucleotides)
+                      for nuc in range(50)])
+
+#DNAStr = validate_seq(randDNAStr)
+DNAStr = 'TCAGAATACTTCAGATGGTCATCTAACGTGAGGTCCAATCGACCGTGGGAACTACGGAACGGCTAGGACTGCCTGCGCCGAAAGGAATCCTTGCACGAACTCAAGTGTTAGGCTCGGATCGTGGCCTAAAGGTTATTTATTTACTCACCCGTCAGTAAACTATACTGTGTTTTGTCAAATCAGCGACACGAACCGTGAAAACACTCCCTAAGAGACTCTAACCCTATCGTGATGTGAAGTCAGACTACAATAGAACTTTCTTAGTTTCAGACAAGGATGCGGTGTACCATGACAATACGCTAAGCGGCCCTGTCGTAAACACTGGTAGAATTTTCGAGCCGGGACACGGCTGGAGCGTACCGATGCTGCGACGCACCTCCCGCCATACTTGCTACGTCATAGCCATTCCAGCAGGGTCCGGCTAATTACAAATCGAACTCCCGCCCTCCGTTTTTGGGACCCGCCTCGTAAGCTGGCAGAGCACAGCGAGTATGTATTACCGTTCCATGAAGCTTGCGGCCGGTGGTCATATCAGTGGGTGTCCTAGGTCCGTAAGTTCTGGCTTCTCGGTAACTACACACATAACCTTTCCGTAGCCCGTGTCGTTCAACACTCACCAGCACGGTGCATGTAGATAGTGGCGGAAACACCTACAACTAATAGCGGCGTAAAACGGGTGTTTCATTTCAACCGCCCCCTCTGAGTCCTACGAAGTCTTATCTGGCAAGTCGACCAGCTAAGTAAATATACTGTGAAACGTTTCTCATACAATACGTCAGGAATAGTAGCCTGGGTCCTTATGGCCTCGAGGCATGCTGGGCACGTTGGTTCCGCCAAGACACCGGGACCACGCTACATTTCGACAACTACGGCTCCTATCAC'
+
+
+print(f'\nSequence: {DNAStr}\n')
+print(f'[1] + Sequence Length: {len(DNAStr)}\n')
+print(f'[2] + Nucleotide Frequency: {nucleotide_frequency(DNAStr)}\n')
+
+print(f'[3] + DNA/RNA Transcription: {transcription(DNAStr)}\n')
+
+print(f"[4] + DNA String + Complement + Reverse Complement:\n5' {DNAStr} 3'")
+print(f"   {''.join(['|' for c in range(len(DNAStr))])}")
+print(f"3' {reverse_complement(DNAStr)[::-1]} 5' [Complement]")
+print(f"5' {reverse_complement(DNAStr)} 3' [Rev. Complement]\n")
+
+print(f'[5] + GC Content: {gc_content(DNAStr)}%\n')
+print(
+    f'[6] + GC Content in Subsection k=5: {gc_content_subsec(DNAStr, k=5)}\n')
+
+print(
+    f'[7] + Aminoacids Sequence from DNA: {translate_seq(DNAStr, 0)}\n')
+
+print(
+    f'[8] + Codon frequency (L): {codon_usage(DNAStr, "L")}\n')
+
+print('[9] + Reading_frames:')
+for frame in gen_reading_frames(DNAStr):
+    print(frame)
+
+print(proteindata)
